bd5919a38b00123011379407e2f4b096d004b1eb
Aditya-Byte <adityabyte@gmail.com>
2025-06-21 11:47:30 +0530

    chore: changed the topic and added logs for testing with consumer

--- Kafka-Producer/src/main/java/com/kafka_demo/service/KafkaMessagePublisher.java
+++ Kafka-Producer/src/main/java/com/kafka_demo/service/KafkaMessagePublisher.java
@@ -1,5 +1,7 @@
 package com.kafka_demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -13,20 +15,22 @@ public class KafkaMessagePublisher {
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
+    Logger log = LoggerFactory.getLogger(KafkaMessagePublisher.class);
+
     public void sendMessageToTopic(String message) {
         // The return type of the send method is the Completable future (asynchronous method).
         // So if you want to block the main thread till the message is being sent in that case.
         // You can do the future.get().
-        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("aditya1", message);
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("aditya", message);
         // future.get() // If we do this this will slow down the producer.
         // Since kafka is a fast messaging platform, so it's better to handle messages asynchronously.
         // So that the subsequent message didn't wait for the result of the previous messages.
         // We can do this with the help of a callback implementation.
         future.whenComplete((result, exception) -> {
             if (exception == null) {
-                System.out.println("Sent Message= [" + message + "] with offset=[" + result.getRecordMetadata().offset() + "] and with Partition=[" + result.getRecordMetadata().partition() + "]");
+                log.info("Sent Message=[{}] with offset=[{}] and with Partition=[{}]", message, result.getRecordMetadata().offset(), result.getRecordMetadata().partition());
             } else {
-                System.out.println("Unable to send message["+ message + "] due to: [" + exception.getMessage() + "]");
+                log.error("Unable to send message=[{}] due to: [{}]", message, exception.getMessage());
             }
         });
 
