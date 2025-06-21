dbc2300b31b1e98bbb4d8a72374a43d6b3df60e9
Aditya-Byte <adityabyte@gmail.com>
2025-06-21 14:21:54 +0530

    Moved from application.properties config to java based config for more flexibility

--- Kafka-Producer/src/main/java/com/kafka_demo/config/KafkaProducerConfig.java
+++ Kafka-Producer/src/main/java/com/kafka_demo/config/KafkaProducerConfig.java
@@ -1,8 +1,17 @@
 package com.kafka_demo.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 // If you don't want to create the topic explicitly by the CLI command.
 // In that case, spring boot provides a way by which you can create a topic by the
@@ -14,5 +23,31 @@ public class KafkaProducerConfig {
     @Bean
     public NewTopic createTopic() {
         return new NewTopic("aditya", 3, (short) 1);
+    }
+
+    // Configuration as per java beans.
+    // If you don't want to define the things in the application.properties file.
+    // Then you could also define it in the config class by creating beans for more flexibility.
+
+    // Firstly you have to define the producerConfig bean which returns a map.
+    @Bean
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    // Now you have to define the producer factory bean.
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    // Now you have to define the KafkaTemplate object to set these things.
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
