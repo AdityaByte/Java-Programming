9f5e4eb7168ea458492f0d3cc5fecedb00b74292
Aditya-Byte <adityabyte@gmail.com>
2025-06-21 11:48:14 +0530

    successfully implemented kafka consumer and consume messages via making a consumer group

---
+++ Kafka-Consumer/src/main/java/com/kafka_demo/consumer/KafkaMessageListener.java
@@ -0,0 +1,42 @@
+package com.kafka_demo.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessageListener {
+
+    Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "aditya", groupId = "group-1")
+    public void consumer1(String message) {
+        log.info("[consumer1] Consumed message: {}", message);
+    }
+
+    // Note: In the upper case I had tested the consumer with only one partition likely i am publishing the message to only
+    // one partition and the only consumer is listening to them.
+    // But in the next example we will create a consumer group having the same group Id and the
+    // zookeeper and coordinator will assign the partition to each consumer.
+
+    // Example: we have a topic which has 3 partitions and we have 4 consumers.
+    // we are producing large set of data so the coordinator assigned individual consumers
+    // for consuming the data.
+    // Let use see how many data is being added in a particular partition and how the consumer is consumed it.
+
+    @KafkaListener(topics = "aditya", groupId = "group-1")
+    public void consumer2(String message) {
+        log.info("[consumer2] Consumed message: {}", message);
+    }
+
+    @KafkaListener(topics = "aditya", groupId = "group-1")
+    public void consumer3(String message) {
+        log.info("[consumer3] Consumed message: {}", message);
+    }
+
+    @KafkaListener(topics = "aditya", groupId = "group-1")
+    public void consumer4(String message) {
+        log.info("[consumer4] Consumed message: {}", message);
+    }
+}
